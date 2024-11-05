3a35ab01185a62b5888a7bc4dd1539f82e70974a
Ryusei <jaurunaruru1234@gmail.com>
2024-11-05 16:50:30 +0900

    Initial Commit

---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by 川原龍成 on 2024/11/05.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
