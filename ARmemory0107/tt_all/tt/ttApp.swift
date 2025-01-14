6b31eb21f93998b677e2909997dda15c568c8b33
KkzHayato <e225702@ie.u-ryukyu.ac.jp>
2025-01-14 16:49:32 +0900

    merge complete

---
+++ ARmemory0107/tt_all/tt/ttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttApp.swift
+//  tt
+//
+//  Created by 伊佐のの on 2025/01/07.
+//
+
+import SwiftUI
+
+@main
+struct ttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
